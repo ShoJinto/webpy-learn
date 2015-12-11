cfdca3a81cc2e867d23f8dec417fd016d3d36f76
ShoJinto <ShoJinto@gmail.com>
2015-12-11 21:35:59 +0800

    sync home's code

--- code.py
+++ code.py
@@ -1,33 +1,50 @@
 #!/usr/bin/env python
 # coding:utf8
+
 import web
 
-render = web.template.render('tpl/')  # 定义网页模板对象,用于网页渲染
-db = web.database(dbn='mysql', user='tdo', pw='tdo', db='tdo')  # 创建数据对象,用于操作数据
+
+render = web.template.render('tpl/') #定义渲染模版在项目中的位置
+
+db = web.database(dbn='mysql',user='tdo',pw='tdo',db='tdo') #创建数据对象，用于指定数据库类型，连接权限
 
 urls = (
-    '/', 'index',
-    '/add', 'add'
-)
+	"/","index",
+	"/add","add"
+	)
+
+# class index(object):
+# 	"""docstring for index"""
+# 	def  GET(self): #利用GET方式提供页面
+# 		# return "Hello Webpy!"
+
+# 		# name = 'ShoJinto'
+# 		# return render.index(name)  #此处的index是指的模版中的index.html
+# 		i = web.input(name=None)  #使用web.input方法用接收用户输入，即URL传参
+# 		return render.index(i.name)  #访问形式 http://127.0.0.1/?name=shojinto
+
+# class index(object):
+# 	"""docstring for index"""
+# 	def GET(self, name): # 定义GET方法接收URL参数传入
+# 		return render.index(name) #访问形式 http://127.0.0.1/shojinto 
+
+class index(object):
+	"""docstring for index"""
+	def GET(self):
+		tdos = db.select('tdo')
+		return render.index(tdos)
+
+class add(object):
+	"""docstring for add"""
+	def POST(self):
+		i = web.input()  #web.input 保存的时候form提交过来的所有数据
+		n = db.insert('tdo',title=i.title)  # 其实这里可以不定义变量n 而直接执行db.insert()方法，该方法返回的是出入导数据后改行的行号
+		# return render.n(i)
+		raise web.seeother('/')
 
 
-class index:
-    def GET(self):
-        # i = web.input(name=None) #web.input接受客户端所有的输入数据,包括form和url传参过来的
-        # return render.index(name) #此处的index是指的模板目录tpl下的index.html
-
-        tdos = db.select('todo')
-        return render.index(tdos)
 
 
-class add:
-    def POST(self):
-        i = web.input()
-        n = db.insert('todo', title=i.title)  # 此处声明的变量n用于保存插入数据的行号,如果不需要返回也可以不用定义声明变量.
-        raise web.seeother('/')               # db.insert('todo',title=i.title) 这样也行
-
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
-
+if __name__ == "__main__":  #启动web服务
+	app = web.application(urls,globals())
+	app.run()
