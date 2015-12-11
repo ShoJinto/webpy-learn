cf280be8b67747ba035be44f34300bd24750b9d5
ShoJinto <ShoJinto@gmail.com>
2015-12-11 21:58:12 +0800

    sync home's code

--- static/db.sql
+++ static/db.sql
@@ -2,8 +2,7 @@ CREATE TABLE tdo (
   id serial primary key,
   title text,
   created timestamp default now(),
-  done tinyint default 0    
+  done tinyint(1) default 0
   );
-
 
 INSERT INTO tdo (title) VALUES ('Learn web.py');
